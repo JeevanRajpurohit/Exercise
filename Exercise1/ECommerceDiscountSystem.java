4e7e08170ae031b046d24c9a0c217efe8c705106
Jeevan Rajpurohit <jivanpurohit1106@gmail.com>
2025-02-14 16:34:33 +0530

    Added Functionality to get to price

--- Exercise1/ECommerceDiscountSystem.java
+++ Exercise1/ECommerceDiscountSystem.java
@@ -5,6 +5,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ECommerceDiscountSystem {
+    public static double calculateTotalPrice(List<Product> productList){
+        double totolPrice=0.0;
+        for(Product product : productList){
+            totolPrice+=(product.price);
+        }
+        return totolPrice;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Product> productList = new ArrayList<>();
@@ -17,15 +24,28 @@ public class ECommerceDiscountSystem {
         productList.add(laptop);
         productList.add(tshirt);
 
+        System.out.print("Please Enter 1 to see , Total price of product before discount : ");
+        int value = scanner.nextInt();
+        if(value==1){
+            System.out.println(calculateTotalPrice(productList));
+        }
+
         for (Product product : productList) {
             System.out.print("Enter the Discount percentage for " + product.name + " : ");
             double discount = scanner.nextDouble();
             ((Discountable) product).applyDiscount(discount);
         }
 
+
         System.out.println("Final Product Details:");
         for (Product product : productList) {
             System.out.println(product.getDetails());
+        }
+
+        System.out.print("Please Enter 1 to see , Total price of product after discount : ");
+        int value1 = scanner.nextInt();
+        if(value1==1){
+            System.out.println(calculateTotalPrice(productList));
         }
 
         scanner.close();
